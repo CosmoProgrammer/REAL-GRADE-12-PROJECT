cbb3e493144fc8b7effbd8253a2edc62b5a552bd
rnpsmun2022 <rnpsmun2022@outlook.com>
2023-11-08 23:51:56 +0530

    Dynamic Rendering

---
+++ client/src/containers/DynamicTable.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const DynamicTable = ({ schema, data }) => {
+  const buildHeaders = () => {
+    return schema.map((column, i) => {
+      return (
+        <th key={i}>
+          {column.name}
+          {column.subColumns.length > 0 && (
+            <tr>
+              {column.subColumns.map((subColumn, j) => (
+                <th key={`${i}-${j}`}>{subColumn.name}</th>
+              ))}
+            </tr>
+          )}
+        </th>
+      );
+    });
+  };
+
+  const buildBody = () => {
+    return data.map((row, i) => {
+      return (
+        <tr key={i}>
+          {schema.map((column, j) => {
+            if (column.subColumns.length > 0) {
+              return (
+                <td key={`${i}-${j}`}>
+                  <tr>
+                    {column.subColumns.map((subColumn, k) => (
+                      <td key={`${i}-${j}-${k}`}>{row[j][k]}</td>
+                    ))}
+                  </tr>
+                </td>
+              );
+            } else {
+              return <td key={`${i}-${j}`}>{row[j]}</td>;
+            }
+          })}
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>{buildHeaders()}</tr>
+      </thead>
+      <tbody>{buildBody()}</tbody>
+    </table>
+  );
+};
+
+export default DynamicTable;
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,7 +1,38 @@
 import React from "react";
+import Table from "./DynamicTable";
 
-const HomePage = () => {
-  return <div className="home">Home</div>;
+const HomePage = (props) => {
+  //Insert method to retrieve data from the server here
+  const schema = [
+    { name: "ID", type: "number", subColumns: [] },
+    { name: "Public/NonPublic", type: "string", subColumns: [] },
+    {
+      name: "Qty",
+      type: "number",
+      subColumns: [
+        { name: "Public", type: "number" },
+        { name: "Private", type: "number" },
+      ],
+    },
+    { name: "Industry", type: "string", subColumns: [] },
+    {
+      name: "Types",
+      type: "string",
+      subColumns: [
+        { name: "Taste", type: "string" },
+        { name: "Colour", type: "string" },
+        { name: "Smell", type: "string" },
+      ],
+    },
+  ];
+
+  const data = [
+    [1, "Public", [5, 10], "Cooking", ["Sweet", "Green", "Pungent"]],
+    [2, "Public", [6, 20], "Baking", ["Sour", "Blue", "Pleasant"]],
+    [3, "NonPublic", [5, 5], "Cooking", ["Spicy", "Green", "None"]],
+  ];
+
+  return <Table schema={schema} data={data} />;
 };
 
 export default HomePage;
